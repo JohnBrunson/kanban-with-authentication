d2f5ebfed56a9476955ef8c9cf66724fc19dbc8a
JohnBrunson <JohnFBrunson@gmail.com>
2025-02-04 23:23:25 -0800

    Making ready to deploy

--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -1,20 +1,21 @@
-import { useEffect, useState, useLayoutEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState, useLayoutEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import { retrieveTickets, deleteTicket } from '../api/ticketAPI';
-import ErrorPage from './ErrorPage';
-import Swimlane from '../components/Swimlane';
-import { TicketData } from '../interfaces/TicketData';
-import { ApiMessage } from '../interfaces/ApiMessage';
+import { retrieveTickets, deleteTicket } from "../api/ticketAPI";
+import ErrorPage from "./ErrorPage";
+import Swimlane from "../components/Swimlane";
+import { TicketData } from "../interfaces/TicketData";
+import { ApiMessage } from "../interfaces/ApiMessage";
 
-import auth from '../utils/auth';
+import auth from "../utils/auth";
 
-const boardStates = ['Todo', 'In Progress', 'Done'];
+const boardStates = ["Todo", "In Progress", "Done"];
 
 const Board = () => {
   const [tickets, setTickets] = useState<TicketData[]>([]);
   const [error, setError] = useState(false);
   const [loginCheck, setLoginCheck] = useState(false);
+  const navigate = useNavigate();
 
   // const checkLogin = () => {
   //   if(auth.loggedIn()) {
@@ -27,7 +28,7 @@ const Board = () => {
       setLoginCheck(true);
     } else {
       auth.logout();
-      navigate('/login');
+      navigate("/login");
     }
   };
 
@@ -36,12 +37,12 @@ const Board = () => {
       const data = await retrieveTickets();
       setTickets(data);
     } catch (err) {
-      console.error('Failed to retrieve tickets:', err);
+      console.error("Failed to retrieve tickets:", err);
       setError(true);
     }
   };
 
-  const deleteIndvTicket = async (ticketId: number) : Promise<ApiMessage> => {
+  const deleteIndvTicket = async (ticketId: number): Promise<ApiMessage> => {
     try {
       const data = await deleteTicket(ticketId);
       fetchTickets();
@@ -49,14 +50,14 @@ const Board = () => {
     } catch (err) {
       return Promise.reject(err);
     }
-  }
+  };
 
   useLayoutEffect(() => {
     checkLogin();
   }, []);
 
   useEffect(() => {
-    if(loginCheck) {
+    if (loginCheck) {
       fetchTickets();
     }
   }, [loginCheck]);
@@ -67,34 +68,32 @@ const Board = () => {
 
   return (
     <>
-    {
-      !loginCheck ? (
-        <div className='login-notice'>
-          <h1>
-            Login to create & view tickets
-          </h1>
-        </div>  
+      {!loginCheck ? (
+        <div className="login-notice">
+          <h1>Login to create & view tickets</h1>
+        </div>
       ) : (
-          <div className='board'>
-            <button type='button' id='create-ticket-link'>
-              <Link to='/create' >New Ticket</Link>
-            </button>
-            <div className='board-display'>
-              {boardStates.map((status) => {
-                const filteredTickets = tickets.filter(ticket => ticket.status === status);
-                return (
-                  <Swimlane 
-                    title={status} 
-                    key={status} 
-                    tickets={filteredTickets} 
-                    deleteTicket={deleteIndvTicket}
-                  />
-                );
-              })}
-            </div>
+        <div className="board">
+          <button type="button" id="create-ticket-link">
+            <Link to="/create">New Ticket</Link>
+          </button>
+          <div className="board-display">
+            {boardStates.map((status) => {
+              const filteredTickets = tickets.filter(
+                (ticket) => ticket.status === status
+              );
+              return (
+                <Swimlane
+                  title={status}
+                  key={status}
+                  tickets={filteredTickets}
+                  deleteTicket={deleteIndvTicket}
+                />
+              );
+            })}
           </div>
-        )
-    }
+        </div>
+      )}
     </>
   );
 };
